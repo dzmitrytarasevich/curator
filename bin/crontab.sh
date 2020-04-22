2fd80d8c7103c9d8b8fb81452804bf04eae25232
Dzmitry Tarasevich <dzmitry.tarasevich@seavus.com>
2020-04-22 13:13:40 +0300

    changes to test allocation

--- bin/crontab.sh
+++ bin/crontab.sh
@@ -3,7 +3,9 @@
 # Added a cronjob in a new crontab
 echo "* * * * * /opt/bitnami/python/bin/curator --config=/curator/curator.yml /curator/actions/create_index.yml >> /curator/curator.log 2>&1
 # just empty line
-*/2 * * * * /opt/bitnami/python/bin/curator --config=/curator/curator.yml /curator/actions/allocate_and_delete.yml >> /curator/curator.log 2>&1
+*/2 * * * * /opt/bitnami/python/bin/curator --config=/curator/curator.yml /curator/actions/allocate.yml >> /curator/curator.log 2>&1
+# just empty line
+*/6 * * * * /opt/bitnami/python/bin/curator --config=/curator/curator.yml /curator/actions/delete_index.yml >> /curator/curator.log 2>&1
 # just empty line" > /etc/crontab
 
 # Registering the new crontab

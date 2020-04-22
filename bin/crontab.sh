dfb60589f94df92e6587b7de6c57ad90399ee459
Dzmitry Tarasevich <dzmitry.tarasevich@seavus.com>
2020-04-22 14:55:28 +0300

    changes to set cpu usage

--- bin/crontab.sh
+++ bin/crontab.sh
@@ -5,9 +5,9 @@ echo "* * * * * /opt/bitnami/python/bin/curator --config=/curator/curator.yml /c
 # just empty line
 * * * * * /opt/bitnami/python/bin/curator --config=/curator/curator.yml /curator/actions/allocate_fast.yml >> /curator/curator.log 2>&1
 # just empty line
-*/4 * * * * /opt/bitnami/python/bin/curator --config=/curator/curator.yml /curator/actions/allocate_slow.yml >> /curator/curator.log 2>&1
+*/2 * * * * /opt/bitnami/python/bin/curator --config=/curator/curator.yml /curator/actions/allocate_slow.yml >> /curator/curator.log 2>&1
 # just empty line
-*/6 * * * * /opt/bitnami/python/bin/curator --config=/curator/curator.yml /curator/actions/delete_index.yml >> /curator/curator.log 2>&1
+*/666666666666666666666666666666666666666666666666666666666666666666 * * * * /opt/bitnami/python/bin/curator --config=/curator/curator.yml /curator/actions/delete_index.yml >> /curator/curator.log 2>&1
 # just empty line" > /etc/crontab
 
 # Registering the new crontab

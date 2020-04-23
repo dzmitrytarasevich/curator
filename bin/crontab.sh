9c5e7401ead7f344c3f8c0f8056a2f49c8eae8e3
Dzmitry Tarasevich <dzmitry.tarasevich@seavus.com>
2020-04-23 12:11:51 +0300

    changes in cron to prevent overlay

--- bin/crontab.sh
+++ bin/crontab.sh
@@ -2,17 +2,17 @@
 
 # Added a cronjob in a new crontab
 echo "* * * * * /opt/bitnami/python/bin/curator --config=/curator/curator.yml /curator/actions/create_index.yml >> /curator/curator.log 2>&1
-# just empty line
+# create index every minute
 #* * * * * /opt/bitnami/python/bin/curator --config=/curator/curator.yml /curator/actions/allocate_fast.yml >> /curator/curator.log 2>&1
 # just empty line
 #* * * * * /opt/bitnami/python/bin/curator --config=/curator/curator.yml /curator/actions/allocate_slow.yml >> /curator/curator.log 2>&1
 # just empty line
-* * * * * /opt/bitnami/python/bin/curator --config=/curator/curator.yml /curator/actions/index_settings_fast.yml >> /curator/curator.log 2>&1
-# just empty line
-* * * * * /opt/bitnami/python/bin/curator --config=/curator/curator.yml /curator/actions/index_settings_slow.yml >> /curator/curator.log 2>&1
-# just empty line
-* * * * * /opt/bitnami/python/bin/curator --config=/curator/curator.yml /curator/actions/delete_index.yml >> /curator/curator.log 2>&1
-# just empty line" > /etc/crontab
+*/2 * * * * /opt/bitnami/python/bin/curator --config=/curator/curator.yml /curator/actions/index_settings_fast.yml >> /curator/curator.log 2>&1
+# allocate index to nodes with attribute fast
+*/3 * * * * /opt/bitnami/python/bin/curator --config=/curator/curator.yml /curator/actions/index_settings_slow.yml >> /curator/curator.log 2>&1
+# allocate index to nodes with attribute slow
+*/5 * * * * /opt/bitnami/python/bin/curator --config=/curator/curator.yml /curator/actions/delete_index.yml >> /curator/curator.log 2>&1
+# delete indexes older than 5 minutes" > /etc/crontab
 
 # Registering the new crontab
 crontab /etc/crontab

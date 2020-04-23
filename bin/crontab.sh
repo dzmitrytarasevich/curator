5e7a275b7663f72be03ab94a52d4915fb4142b02
Dzmitry Tarasevich <dzmitry.tarasevich@seavus.com>
2020-04-23 12:51:45 +0300

    deleted allocate from cron, changed filter type in index_settings*

--- bin/crontab.sh
+++ bin/crontab.sh
@@ -3,10 +3,6 @@
 # Added a cronjob in a new crontab
 echo "* * * * * /opt/bitnami/python/bin/curator --config=/curator/curator.yml /curator/actions/create_index.yml >> /curator/curator.log 2>&1
 # create index every minute
-#* * * * * /opt/bitnami/python/bin/curator --config=/curator/curator.yml /curator/actions/allocate_fast.yml >> /curator/curator.log 2>&1
-# just empty line
-#* * * * * /opt/bitnami/python/bin/curator --config=/curator/curator.yml /curator/actions/allocate_slow.yml >> /curator/curator.log 2>&1
-# just empty line
 */2 * * * * /opt/bitnami/python/bin/curator --config=/curator/curator.yml /curator/actions/index_settings_fast.yml >> /curator/curator.log 2>&1
 # allocate index to nodes with attribute fast
 */3 * * * * /opt/bitnami/python/bin/curator --config=/curator/curator.yml /curator/actions/index_settings_slow.yml >> /curator/curator.log 2>&1

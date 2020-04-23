ee2b91274630ac2426001f6b803e846d4a3da1f5
Dzmitry Tarasevich <dzmitry.tarasevich@seavus.com>
2020-04-23 11:51:34 +0300

    changes to test index_settings

--- bin/crontab.sh
+++ bin/crontab.sh
@@ -3,11 +3,15 @@
 # Added a cronjob in a new crontab
 echo "* * * * * /opt/bitnami/python/bin/curator --config=/curator/curator.yml /curator/actions/create_index.yml >> /curator/curator.log 2>&1
 # just empty line
-* * * * * /opt/bitnami/python/bin/curator --config=/curator/curator.yml /curator/actions/allocate_fast.yml >> /curator/curator.log 2>&1
+#* * * * * /opt/bitnami/python/bin/curator --config=/curator/curator.yml /curator/actions/allocate_fast.yml >> /curator/curator.log 2>&1
 # just empty line
-* * * * * /opt/bitnami/python/bin/curator --config=/curator/curator.yml /curator/actions/allocate_slow.yml >> /curator/curator.log 2>&1
+#* * * * * /opt/bitnami/python/bin/curator --config=/curator/curator.yml /curator/actions/allocate_slow.yml >> /curator/curator.log 2>&1
 # just empty line
-*/6 * * * * /opt/bitnami/python/bin/curator --config=/curator/curator.yml /curator/actions/delete_index.yml >> /curator/curator.log 2>&1
+* * * * * /opt/bitnami/python/bin/curator --config=/curator/curator.yml /curator/actions/index_settings_fast.yml >> /curator/curator.log 2>&1
+# just empty line
+* * * * * /opt/bitnami/python/bin/curator --config=/curator/curator.yml /curator/actions/index_settings_slow.yml >> /curator/curator.log 2>&1
+# just empty line
+* * * * * /opt/bitnami/python/bin/curator --config=/curator/curator.yml /curator/actions/delete_index.yml >> /curator/curator.log 2>&1
 # just empty line" > /etc/crontab
 
 # Registering the new crontab
